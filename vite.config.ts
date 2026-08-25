2d16a1fff6f956372be35f7640d0f56b9e7c459c
huylongnguyendev <nguyenhuylong.dev@gmail.com>
2026-08-25 11:47:51 -0400

    feat: add PWA support with vite-plugin-pwa and update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,48 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
-import neon from './neon-vite-plugin.ts'
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import neon from "./neon-vite-plugin.ts";
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
-    neon,
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-  ],
-})
+	resolve: { tsconfigPaths: true },
+	plugins: [
+		devtools(),
+		nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+		neon,
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+		VitePWA({
+			registerType: "autoUpdate",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+			manifest: {
+				id: "/",
+				name: "Concrete Pressing Admin",
+				short_name: "AdminApp",
+				description: "Hệ thống quản lý ép cọc bê tông",
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				display: "standalone",
+				start_url: "/",
+				icons: [
+					{
+						src: "/pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
 
-export default config
+export default config;
