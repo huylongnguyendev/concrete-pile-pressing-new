255f6f8033e49b3d7134f6d09e345712dd92c846
huylongnguyendev <nguyenhuylong.dev@gmail.com>
2026-09-07 00:52:50 -0400

    fix: update phone number retrieval and add messenger link in contact data

--- src/components/base/header/interactive/ContactCta.tsx
+++ src/components/base/header/interactive/ContactCta.tsx
@@ -1,13 +1,12 @@
-import { useAppStore } from "@lavaz/store";
 import { PhoneCallIcon } from "lucide-react";
 import { Button } from "#/components/ui/button";
-import { store } from "#/store/store";
+import contact from "#/data/json/contact.json" with { type: "json" };
 
 export function ContactCta() {
-	const [phoneNumber] = useAppStore(store.company, (s) => s.phoneNumber);
+	const { phoneNumber } = contact;
 	return (
 		<Button asChild>
-			<a href={`to:${phoneNumber[0]?.number ?? "+84967386080"}`}>
+			<a href={`tel:${phoneNumber}`}>
 				<PhoneCallIcon />
 				<span className="max-lg:hidden">Tư vấn Miễn phí</span>
 			</a>
